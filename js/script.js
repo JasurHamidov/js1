4b71da1a4ad2fe92ec8090baffa622ab0e2ebfed
JasurHamidov <127729542+JasurHamidov@users.noreply.github.com>
2023-03-20 03:24:17 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let ex1 = "exercise1"
+console.log(ex1);
+let balance = 25000
+let car = 4480 
+let food = 890
+let spending = car + food
+let remainder = balance - spending
+
+
+let invoice = 5500 
+let stock = 4200 
+let income = invoice + stock 
+let totalbalance = remainder + income 
+console.log(totalbalance);
+
+let ex2 = "exercise2"
+console.log(ex2);
+let name = 'jasur'
+let surname = 'Hamidov' 
+let otchestvo = 'Hamidovich'
+let person = name + ' ' + surname + ' ' + otchestvo 
+console.log(person);
+
+let ex3 = "exercise3"
+console.log(ex3);
+let monday = "1. monday"
+let tuesday = "2. tuesday"
+let wednesday = "3. wednesday"
+let thusday = "4. thursday"
+let friday = "5. friday"
+let saturday = "6. saturday"
+let sunday = "7. sunday"
+let week = monday + ' ' + thusday + ' ' + wednesday + ' ' + thusday + ' ' + friday + ' ' + saturday + ' ' + sunday
+console.log(week);
